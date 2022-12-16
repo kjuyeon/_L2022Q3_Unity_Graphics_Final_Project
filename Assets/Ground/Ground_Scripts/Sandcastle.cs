c3700b95555547b1f3af7a72a8adb7fb0ce6c10b
이예원 <20181038@sungshin.ac.kr>
2022-12-17 05:09:10 +0900

    add Audio, Pyramid
    .

--- Assets/Ground/Ground_Scripts/Sandcastle.cs
+++ Assets/Ground/Ground_Scripts/Sandcastle.cs
@@ -1,46 +1,99 @@
-using System.Collections; 
-using System.Collections.Generic; 
-using UnityEngine; 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class Sandcastle : MonoBehaviour 
-{ 
-    Vector3 V0, V1, V2, V3, V4; 
-    Vector3[] newVertices; 
-    int[] newTriangles; 
+public class Sandcastle : MonoBehaviour
+{
+	Vector3 V0, V1, V2, V3, V4;
+	Vector3[] newVertices;
+	int[] newTriangles;
+	Vector3[] normals;
 
-    void Start() 
-    { 
-        V0 = new Vector3(-0.5f, 0, -0.5f); 
-        V1 = new Vector3(-0.5f, 0, 0.5f); 
-        V2 = new Vector3(0.5f, 0, 0.5f); 
-        V3 = new Vector3(0.5f, 0, -0.5f); 
-        V4 = new Vector3(0, 1, 0); 
+    public Texture Sand;
 
-        newVertices = new Vector3[] 
-        { 
-            V0, V1, V2, V3, V4 
-        }; 
+	void Start()
+	{
+		V0 = new Vector3(-0.5f, 0, -0.5f);
+		V1 = new Vector3(-0.5f, 0, 0.5f);
+		V2 = new Vector3(0.5f, 0, 0.5f);
+		V3 = new Vector3(0.5f, 0, -0.5f);
+		V4 = new Vector3(0, 1, 0);
 
-        newTriangles = new int[]  
-        { 
-            0, 2, 1, 
-            0, 3, 2, 
-            0, 4, 3, 
-            3, 4, 2, 
-            2, 4, 1, 
-            1, 4, 0 
-        }; 
+		newVertices = new Vector3[]
+		{
+			// Bottom
+			V0, V1, V2, V3,
 
-        gameObject.AddComponent<MeshFilter>(); 
-        gameObject.AddComponent<MeshRenderer>(); 
+			// Front
+			V2, V4, V1,
 
-        Mesh mesh = new Mesh(); 
-        GetComponent<MeshFilter>().mesh = mesh; 
-        mesh.vertices = newVertices; 
-        mesh.triangles = newTriangles; 
+			// Left
+			V3, V4, V2,
 
-        Shader DefaultShader = Shader.Find("Standard"); 
-        Material DefaultMaterial = new Material(DefaultShader); 
-        gameObject.GetComponent<Renderer>().material = DefaultMaterial; 
-    } 
-} 
+			// Right
+			V1, V4, V0,
+
+			// Back
+			V0, V4, V3				
+		};
+
+		newTriangles = new int[]
+		{
+			// Bottom
+			0, 2, 1,
+			0, 3, 2,
+
+			// Front
+			4, 5, 6,
+
+			// Left
+			7, 8, 9,
+
+			// Right
+			10, 11,12,
+
+			// Back
+			13, 14, 15
+		};
+
+		Vector3 Down = Vector3.down;
+		Vector3 Front = Vector3.forward;
+		Vector3 Left = Vector3.left;
+		Vector3 Right = Vector3.right;
+		Vector3 Back = Vector3.back;
+
+		normals = new Vector3[]
+		{
+			// Bottom
+			Down, Down, Down, Down,
+ 
+			// Front
+			Front, Front, Front,
+ 
+			// Left
+			Left, Left, Left,			
+ 
+			// Right
+			Right, Right, Right,
+
+			// Back
+			Back, Back, Back
+		};
+
+        gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+
+		Mesh mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		mesh.vertices = newVertices;
+		mesh.triangles = newTriangles;
+		mesh.normals = normals;
+
+		Shader DefaultShader = Shader.Find("Standard");
+		Material DefaultMaterial = new Material(DefaultShader);
+        DefaultMaterial.mainTexture = Sand;
+		gameObject.GetComponent<Renderer>().material = DefaultMaterial;
+
+	}
+
+}
--- Assets/Ground/Ground_Scripts/TrashcanController.cs
+++ Assets/Ground/Ground_Scripts/TrashcanController.cs
@@ -6,16 +6,19 @@ public class TrashcanController : MonoBehaviour
 {   
     public GameObject Pivot;
     public GameObject TrashcanPoint;
+    AudioSource audioSoure;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSoure = GetComponent<AudioSource>();
     }
 
     private void OnMouseDown()
     {
         Pivot.GetComponent<Animator>().SetInteger("Can", 1);
         TrashcanPoint.SetActive(false);
+        audioSoure.Play();        
     }
+    
 }
